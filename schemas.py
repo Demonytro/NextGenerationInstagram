fec2e685065c52231521ab50029a671b1efd8329
Nataleia Orlovska <101810309+Nataleia08@users.noreply.github.com>
2023-06-13 20:20:46 +0300

    final cloudinary

--- schemas.py
+++ schemas.py
@@ -1,14 +1,73 @@
-from pydantic import BaseModel, EmailStr, PastDate, Field, FutureDate
-from datetime import datetime, date
-from typing import Optional
 
+from typing import List
+
+from fastapi import UploadFile
+from pydantic import BaseModel, constr
+from pydantic.types import conlist
+
+
+class ImageCreateRequest(BaseModel):
+    description: str = None
+    tags: conlist(constr(max_length=50), min_items=1, max_items=5) = []
+
+
+class ImageUpdateRequest(BaseModel):
+    description: str
+    tags: conlist(constr(max_length=50), min_items=1, max_items=5) = []
+
+
+class ImageUpdateImageRequest(BaseModel):
+    image: UploadFile
+
+
+class ImageUpdateTagsRequest(BaseModel):
+    tags: conlist(constr(max_length=50), min_items=1, max_items=5) = []
+
+
+class ImageUpdateDescriptionRequest(BaseModel):
+    description: str
+
+
+class ImageResponse(BaseModel):
+    id: int
+    image: str
+    description: str
+    tags: List[str] = []
+    comments: List[str] = []
+
+
+
+class CommentBase(BaseModel):
+    text: str = Field(max_length=500)
+
+
+class CommentModel(CommentBase):
+    id: int
+    created_at: datetime
+    updated_at: Optional[datetime]
+    user_id: int
+    image_id: int
+    update_status: bool = False
+
+    class Config:
+        orm_mode = True
+
+
+class CommentUpdate(CommentModel):
+    update_status: bool = True
+    updated_at = datetime
+
+    class Config:
+        orm_mode = True
+        
 class ImageResponseCloudinaryModel(BaseModel):
     id: int
     image: str
     is_active: bool
     # created_at: datetime
     # update_at: datetime
-    user_id: int
+    # user_id: int
 
     class Config():
+
         orm_mode = True
---
+++ src/database/models.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+from enum import Enum
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, DateTime, func, Boolean, Text
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Image(Base):
+    __tablename__ = 'images'
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    image = Column(String, nullable=False)
+    description = Column(String)
+    tags = relationship('Tag', secondary='image_tag')
+    comments = relationship('Comment', backref='image')
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+
+    user = relationship('User', backref="image")
+    is_active = Column(Boolean, default=True)
+
+
+class Tag(Base):
+    __tablename__ = 'tags'
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
+    images = relationship('Image', secondary='image_tag')
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+# <<<<<<< Daniil
+    text = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    user_id = Column('user_id', ForeignKey(
+        'users.id', ondelete='CASCADE'), default=None)
+    image_id = Column('image_id', ForeignKey(
+        'images.id', ondelete='CASCADE'), default=None)
+    update_status = Column(Boolean, default=False)
+
+    user = relationship('User', backref="comments")
+    image = relationship('Image', backref="comments")
+# =======
+#     image_id = Column(Integer, ForeignKey('images.id'))
+#     content = Column(String, nullable=False)
+#     created_at = Column(DateTime, default=datetime.now)
+#     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+# >>>>>>> dev
+
+
+image_tag = Table(
+    'image_tag',
+    Base.metadata,
+    Column('image_id', Integer, ForeignKey('images.id'), primary_key=True),
+    Column('tag_id', Integer, ForeignKey('tags.id'), primary_key=True)
+)
+
+
+class UserRole(str, Enum):
+    USER = "user"
+    MODERATOR = "moderator"
+    ADMIN = "admin"
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
+    role = Column(String(20), default=UserRole.USER)
