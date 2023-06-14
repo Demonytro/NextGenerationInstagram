6e714668cbfb4741616c3e0044b2401210c86dd7
Daniel Zubov <109290535+DanielDDZ@users.noreply.github.com>
2023-06-14 09:35:16 +0300

    Update roles.py

--- src/services/roles.py
+++ src/services/roles.py
@@ -2,12 +2,12 @@ from typing import List
 
 from fastapi import Depends, HTTPException, status, Request
 
-from src.database.models import User, UserRoleEnum
+from src.database.models import User, UserRole
 from src.services.auth import auth_service
 
 
 class RoleChecker:
-    def __init__(self, allowed_roles: List[UserRoleEnum]):
+    def __init__(self, allowed_roles: List[UserRole]):
         self.allowed_roles = allowed_roles
 
     async def __call__(self, request: Request, current_user: User = Depends(auth_service.get_current_user)):
