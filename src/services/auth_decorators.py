0698bcfd20fb27d547033f093e021a6ffccac42c
PolinaDyka0 <115357542+PolinaDyka0@users.noreply.github.com>
2023-06-14 18:35:59 +0300

    add kwargs in auth_decorators

--- src/services/auth_decorators.py
+++ src/services/auth_decorators.py
@@ -1,19 +1,22 @@
 from fastapi import HTTPException, status
 from functools import wraps
 from src.database.models import Image
+
 def has_role(role):
     def decorator(view_func):
         @wraps(view_func)
-        async def wrapper(current_user, image_id=None, db=None):
-            if not image_id:
+        async def wrapper(*args, **kwargs):
+            current_user = kwargs.get('current_user')
+            if not kwargs.get('image_id'):
                 if current_user.role not in role:
                     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Insufficient privileges")
             else:
+                db = kwargs.get('db')
+                image_id = kwargs.get('image_id')
                 image_user_id = db.query(Image).filter(Image.id == image_id).first().user_id
                 if current_user.role not in role and image_user_id != current_user.id:
                     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Insufficient privileges for current user")
             return await view_func(current_user)
         return wrapper
     return decorator
-
 
